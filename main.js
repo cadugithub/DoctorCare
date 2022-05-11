8bc12e44e9b894bb2d4a79f9aa25dd94061da60f
cadugithub <carlosk2k2@gmail.com>
2022-05-11 16:41:39 -0300

    implementation of the function activeMenuCurrentSection

--- main.js
+++ main.js
@@ -3,12 +3,59 @@ onScroll()
 function onScroll(){
   showNavOnScroll()
   showBackToTopOnScroll()
-}
 
-function activateMenuAtCurrentSection(){
-  
-}
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 
+
+}
+function activateMenuAtCurrentSection(section){
+  const targetLine = scrollY + innerHeight / 2 
+
+  //varificar se a seção passou da linha
+  //quais dados vou precisar?
+
+  //topo da seção
+  const sectionTop = section.offsetTop
+
+  //altura da seção
+  const sectionHeight = section.offsetHeight
+ 
+
+  // O topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  //Informações dos dados
+  console.log(
+    "o topo da seção chegou ou passou da linha?",sectionTopReachOrPassedTargetLine
+  )
+
+  //Verificar se a base está abaixo da linha
+  //quais dados vou precisar?
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //O final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  console.log("O final da seção passou da linha?", sectionEndPassedTargetLine)
+
+
+  //limites da seção
+
+  const sectionBaudaries = 
+  sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBaudaries){
+    menuElement.classList.add('active')
+  }
+}
 function showNavOnScroll(){
   if(scrollY == 0){
     navigation.classList.remove('scroll')
@@ -44,3 +91,6 @@ ScrollReveal({
   #about, 
   #about header,
   #about content`);
+
+
+
